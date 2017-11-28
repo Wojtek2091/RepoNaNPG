32b46a906d5b488b8f2d192aa6440031a7a73649
Wojtek <Wojtek.zarnowka@gmail.com>
2017-11-28 15:56:46 +0100

    Zmiana kodu w pliku Gauss.cpp

--- Gauss.cpp
+++ Gauss.cpp
@@ -3,45 +3,11 @@
 #include <cmath>
 
 using namespace std;
-
+bool gauss(int n, double ** AB, double * X);
 const double eps = 1e-12; // sta³a przybli¿enia zera
 
 // Funkcja realizuje algorytm eliminacji Gaussa
 //---------------------------------------------
-bool gauss(int n, double ** AB, double * X)
-{
-
-  int i,j,k;
-  double m,s;
-
-  // eliminacja wspó³czynników
-
-  for(i = 0; i < n - 1; i++)
-  {
-    for(j = i + 1; j < n; j++)
-    {
-      if(fabs(AB[i][i]) < eps) return false;
-      m = -AB[j][i] / AB[i][i];
-      for(k = i + 1; k <= n; k++)
-        AB[j][k] += m * AB[i][k];
-    }
-  }
-
-  // wyliczanie niewiadomych
-
-  for(i = n - 1; i >= 0; i--)
-  {
-    s = AB[i][n];
-    for(j = n - 1; j >= i + 1; j--)
-      s -= AB[i][j] * X[j];
-    if(fabs(AB[i][i]) < eps) return false;
-    X[i] = s / AB[i][i];
-  }
-  return true;
-}
-
-// Program g³ówny
-//---------------
 
 int main()
 {
@@ -83,3 +49,39 @@ int main()
 
   return 0;
 }
+
+bool gauss(int n, double ** AB, double * X)
+{
+
+  int i,j,k;
+  double m,s;
+
+  // eliminacja wspó³czynników
+
+  for(i = 0; i < n - 1; i++)
+  {
+    for(j = i + 1; j < n; j++)
+    {
+      if(fabs(AB[i][i]) < eps) return false;
+      m = -AB[j][i] / AB[i][i];
+      for(k = i + 1; k <= n; k++)
+        AB[j][k] += m * AB[i][k];
+    }
+  }
+
+  // wyliczanie niewiadomych
+
+  for(i = n - 1; i >= 0; i--)
+  {
+    s = AB[i][n];
+    for(j = n - 1; j >= i + 1; j--)
+      s -= AB[i][j] * X[j];
+    if(fabs(AB[i][i]) < eps) return false;
+    X[i] = s / AB[i][i];
+  }
+  return true;
+}
+
+// Program g³ówny
+//---------------
+
